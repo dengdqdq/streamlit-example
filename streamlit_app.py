ed36b6edd9286dcc8272708c7ce0a0124322689e
dengdqdq <48615618+dengdqdq@users.noreply.github.com>
2022-01-21 13:02:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,13 +80,21 @@ st.markdown(txt)
 import numpy as np
 import matplotlib.pyplot as plt
 
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
 x=np.linspace(0,2*np.pi,32)
 fig = plt.figure()
 plt.plot(x, np.sin(x),x,np.cos(x))
-plt.show()
+
+st.pyplot(fig)
+
 plt.plot(x,np.exp(x))
-plt.show()
+st.pyplot(fig)
 
 x=np.linspace(-0.5*np.pi,0.5*np.pi,100)
 plt.plot(x,np.tan(x))
-plt.show()
+st.pyplot(fig)
