4223b0966fae5be0248140c6d5fe6ef57861a8c3
dengdqdq <48615618+dengdqdq@users.noreply.github.com>
2022-01-22 21:03:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,6 +77,33 @@ st.markdown(txt)
 
 
 
+
+
+
+st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("展开"):
+     st.latex(r'''\huge 
+          a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+          \sum_{k=0}^{n-1} ar^k =
+          a \left(\frac{1-r^{n}}{1-r}\right)
+          ''')
+
+
+     st.latex(r'''\Large 
+          a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+          \sum_{k=0}^{n-1} ar^k =
+          a \left(\frac{1-r^{n}}{1-r}\right)
+          ''')
+
+     st.latex(r'''\large 
+          a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+          \sum_{k=0}^{n-1} ar^k =
+          a \left(\frac{1-r^{n}}{1-r}\right)
+          ''')
+
+     st.image("https://static.streamlit.io/examples/dice.jpg")
+   
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -84,6 +111,7 @@ x=np.linspace(0,2*np.pi,32)
 fig = plt.figure()
 plt.plot(x, np.sin(x),x,np.cos(x))
 st.pyplot(fig)
+
 
 # plt.show()
 # plt.plot(x,np.exp(x))
@@ -118,3 +146,43 @@ with st.expander("展开"):
           ''')
 
      st.image("https://static.streamlit.io/examples/dice.jpg")
+
+
+
+f = lambda x : 1/(1+x**2)
+a = 0; b = 5; N = 10
+n = 10 # Use n*N+1 points to plot the function smoothly
+
+x = np.linspace(a,b,N+1)
+y = f(x)
+
+X = np.linspace(a,b,n*N+1)
+Y = f(X)
+
+fig=plt.figure(figsize=(15,5))
+
+plt.subplot(1,3,1)
+plt.plot(X,Y,'b')
+x_left = x[:-1] # Left endpoints
+y_left = y[:-1]
+plt.plot(x_left,y_left,'b.',markersize=10)
+plt.bar(x_left,y_left,width=(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
+plt.title('Left Riemann Sum, N = {}'.format(N))
+
+plt.subplot(1,3,2)
+plt.plot(X,Y,'b')
+x_mid = (x[:-1] + x[1:])/2 # Midpoints
+y_mid = f(x_mid)
+plt.plot(x_mid,y_mid,'b.',markersize=10)
+plt.bar(x_mid,y_mid,width=(b-a)/N,alpha=0.2,edgecolor='b')
+plt.title('Midpoint Riemann Sum, N = {}'.format(N))    
+
+plt.subplot(1,3,3)
+plt.plot(X,Y,'b')
+x_right = x[1:] # Left endpoints
+y_right = y[1:]
+plt.plot(x_right,y_right,'b.',markersize=10)
+plt.bar(x_right,y_right,width=-(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
+plt.title('Right Riemann Sum, N = {}'.format(N))
+
+st.pyplot(fig)
